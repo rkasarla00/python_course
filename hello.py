2ed31753881bd44c081554716c6030c3980efd1b
Rohit <rkasarla00@gmail.com>
2024-06-09 18:35:42 -0500

    Adding Github Lesson Tasks

---
+++ hello.py
@@ -0,0 +1,27 @@
+#Task 1
+
+print("Hello World!")
+
+#Task 2
+name = input("What is your name? ")
+print(f"Hello {name}, it is so nice to meet you!")
+
+#Task 3
+
+num1 = int(input("Enter your first whole number: "))
+num2 = int(input("Enter your second whole number: "))
+
+add = num1+num2
+diff = num1 - num2
+prod = num1 * num2
+quotient = num1/num2
+
+print(f"The sum is: {sum}")
+print(f"The difference is: {diff}")
+print(f"The product is: {prod}")
+print(f"The quotient is: {quotient}")
+
+#Task 4
+age = int(input("How old are you? "))
+new_age = age + 1
+print(f"Next year, you will be {new_age} years old")
