2a67c973b7c32a84828a101b9189af676270208f
Rohit Kasarla <67558534+rkasarla00@users.noreply.github.com>
2024-06-15 19:02:49 -0500

    Create finky_code.py

---
+++ finky_code.py
@@ -0,0 +1,29 @@
+char = '#'
+
+#Start of C
+print(f" {char * 4}")
+print(char)
+print(char)
+print(char)
+print(f" {char * 4}")
+print(" ")
+#Start of O
+print(f" {char * 4}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f" {char * 4}")
+print(" ")
+#Start of D
+print(f"{char * 3}")
+print(f"{char}   {char}")
+print(f"{char}    {char}")
+print(f"{char}   {char}")
+print(f"{char * 3}")
+print(" ")
+#Start of E
+print(f"{char * 5}")
+print(f"{char}")
+print(f" {char * 4}")
+print(f"{char}")
+print(f"{char * 5}")
