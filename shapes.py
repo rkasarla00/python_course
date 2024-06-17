6cbfd87108f40ac0521fcdca9c167732c64b8bb5
Rohit <rkasarla00@gmail.com>
2024-06-17 18:42:51 -0500

    Working on geometry

--- shapes.py
+++ shapes.py
@@ -1,8 +1,51 @@
 #Task 1 : Area of rectangle
 #Task 2: Area of Circle
 #Task 3: Area of Triangle
+
+'''
+1. Define the rectangle, circle, and triangle area calculations as functions.
+2. Create a function named square_perimeter that takes the side length of a square and returns its perimeter.
+3. Write a function named circle_details that takes the radius of a circle as its only argument and prints out both the circumference and the area of the circle.
+4. Write a function named geometry that takes the side length of a square and the radius of a circle. The function should print which shape has a larger perimeter/circumference and which shape has a larger area.
+'''
 import math
 
+
+def rectangle (length, width):
+    return length * width
+
+def circle (radius):
+    return math.pi * math.pow(radius, 2)
+
+def triangle (base, height):
+    return .5 * base * height
+
+def square_perimeter(side):
+    return side * 4
+
+def circle_details(radius):
+    circumference =  math.pi * (radius * 2)
+    return circumference, circle(radius)
+
+def geometry(length, radius):
+    perim_square = square_perimeter(length)
+    circum_circle = math.pi * (radius * 2)
+    area_square = length ** 2
+    area_circle = circle (radius)
+
+    if (perim_square > circum_circle):
+        print("The square has a greater perimiter")
+    elif (perim_square < circum_circle):
+        print("The cirlce has the greater circumference")
+    else:
+        print("They have the same perimiter and circumference")
+
+    if ()
+
+
+
+#print(f"The circle_detaile one prints out both {circle_details(3)}")
+'''
 x = int(input("What is the length of the rectangle? "))
 y = int(input("What is the width of the rectangle? "))
 
@@ -22,3 +65,4 @@ f = int(input("What is the base of the triangle? "))
 area_tri = .5 * d * f
 
 print(f"The area of the triangle is: {area_tri}")
+'''
